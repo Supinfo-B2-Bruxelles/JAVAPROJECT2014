ed518142c51d66f87927928ca6dad5dbefaef9f7
sewelseb <sewelseb@hotmail.com>
2014-02-28 10:34:20 +0100

    sortie et reset du jeu

---
+++ Sokoban/src/Jeu/EcouteDeClavier.java
@@ -0,0 +1,25 @@
+package Jeu;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class EcouteDeClavier implements KeyListener{
+	
+	public void keyTyped(KeyEvent event) {
+	      
+	      
+	    }
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}    
+
+}
